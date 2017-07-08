70d0f4c6231d1c022088fb725abe3a5b002d3de7
Andy Akins <andy_akins@mac.com>
2017-07-08 10:26:59 -0500

    Had to change the api name

--- getService/app/getService.py
+++ getService/app/getService.py
@@ -2,5 +2,5 @@ from connexion.resolver import RestyResolver
 import connexion
 
 app = connexion.App(__name__, 8080, specification_dir='api/')
-app.add_api('getService.yaml', resolver=RestyResolver('api'))
+app.add_api('getService.yaml', resolver=RestyResolver('getService'))
 app.run()
