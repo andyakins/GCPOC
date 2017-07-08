61b7789c9f8a79d1b4739529f2c09771aea6d79f
Andy Akins <andy_akins@mac.com>
2017-07-08 12:32:01 -0500

    Converted services to use database

--- getService/app/api/list.py
+++ getService/app/api/list.py
@@ -1,4 +1,23 @@
-data = ["One","Two","Three"]
+import pymysql.cursors
+import os
 
 def search() -> list:
-    return data
+    connection = pymysql.connect(
+                            host=os.environ['GCPOC_DB_HOST'],
+                            user=os.environ['GCPOC_DB_USER'],
+                            password=os.environ['GCPOC_DB_PASSWORD'],
+                            db=os.environ['GCPOC_DB_DATABASE'],
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.Cursor)
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT instring from gcpoc"
+            cursor.execute(sql)
+            result = [item[0] for item in cursor.fetchall()]
+            print(result)
+    except:
+        return None,500
+    finally:
+        connection.close
+
+    return result,200
--- postService/app/api/add.py
+++ postService/app/api/add.py
@@ -1,2 +1,19 @@
+import pymysql.cursors
+import os
+
 def post(instring):
+    connection = pymysql.connect(
+                            host=os.environ['GCPOC_DB_HOST'],
+                            user=os.environ['GCPOC_DB_USER'],
+                            password=os.environ['GCPOC_DB_PASSWORD'],
+                            db=os.environ['GCPOC_DB_DATABASE'],
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.Cursor)
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO gcpoc (instring) VALUES (%s)"
+            cursor.execute(sql, (instring,))
+        connection.commit()
+    finally:
+        connection.close()
     return instring,201
